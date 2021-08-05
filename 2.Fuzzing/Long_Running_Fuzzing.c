7d459704767bc9a652298e947ec0d41d162b7ad4
MJ-SEO <21600343@handong.edu>
2021-08-05 13:48:32 +0900

    fuzzing

--- 2.Fuzzing/Long_Running_Fuzzing.c
+++ 2.Fuzzing/Long_Running_Fuzzing.c
@@ -10,22 +10,20 @@
 
 int pipes[2];
 int error_pipes[2];
-int pass;
 
 char*
 fuzzer(int max_length, int start, int range){
-	int strlen = (rand()%max_length+1)+1;
-	char* out = (char*)malloc(sizeof(char) * strlen);
-	for(int i=0; i<strlen; i++){
+	int len = (rand()%max_length+1)+1;
+	char* out = (char*)malloc(sizeof(char) * len);
+	for(int i=0; i<len; i++){
 		out[i] = (char)((rand()%range) + start);
 	}
+	out[len-1] = '\0';
 	return out;
 }
 
 void 
 child_proc(char* path){
-	printf("[DEBUG] path:%s ", path);
-	
 	close(pipes[0]);
 	close(error_pipes[0]);
 
@@ -50,21 +48,19 @@ parent_proc(char* tempdir, int num){
 	ssize_t s;
 	char *output = (char*)malloc(sizeof(char) * 50);
 	sprintf(output, "%s/output%d", tempdir, num);
-	FILE* outf = fopen(output, "ab");
+	FILE* outf = fopen(output, "wb");
 
 	while((s = read(pipes[0], buf, 1023)) > 0){
-		buf[s] = 0x0;
-		fwrite(buf, 1, strlen(buf), outf);
+		fwrite(buf, 1, s, outf);
 	}
 	fclose(outf);
 	
 	char *error = (char*)malloc(sizeof(char) * 50);
 	sprintf(error, "%s/error%d", tempdir, num);
-	FILE* errf = fopen(error, "ab");
+	FILE* errf = fopen(error, "wb");
 	
 	while((s =read(error_pipes[0], buf, 1023)) > 0){
-		buf[s] = 0x0;
-		fwrite(buf, 1, strlen(buf), errf);
+		fwrite(buf, 1, s, errf);
 	}
 	fclose(errf);
 	
@@ -78,12 +74,12 @@ parent_proc(char* tempdir, int num){
 void 
 subproc(char* path, char* tempdir, int num){
 
-	if(pipe(pipes) != 0){
+	if(pipe(pipes) < 0){
 		perror("pipe error\n");
 		exit(1);
 	}
 	
-	if(pipe(error_pipes) != 0){
+	if(pipe(error_pipes) < 0){
 		perror("pipe error\n");
 		exit(1);
 	}
@@ -110,11 +106,11 @@ long_running_fuzzing(){
 	char *tempdir = mkdtemp(template);	
 	
 	for(int i=1; i<=100; i++){
-		char *basename = (char*)malloc(sizeof(char) * 10);
+		char basename[10];
 		sprintf(basename, "input%d", i);
 		char *data = fuzzer(100, 32, 32);
 
-		char *my_file = (char*)malloc(sizeof(char) * 50);
+		char my_file[40];
 		strcpy(my_file, tempdir);
 		strcat(my_file, "/");
 		strcat(my_file, basename);
