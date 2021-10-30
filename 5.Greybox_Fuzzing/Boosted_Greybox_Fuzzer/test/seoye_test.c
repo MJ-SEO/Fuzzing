85f3730fe2909fd4c1ee68800394fb506e5aec33
MJ-SEO <MJ_SEO>
2021-10-30 15:45:58 +0900

    Before review

--- 5.Greybox_Fuzzing/Boosted_Greybox_Fuzzer/test/seoye_test.c
+++ 5.Greybox_Fuzzing/Boosted_Greybox_Fuzzer/test/seoye_test.c
@@ -8,7 +8,7 @@ config_setting(test_config_t* config){
 	config->mutation = 1;
 	config->file_name = 1;
 
-	config->trial = 150000;
+	config->trial = 1000;
 
 	int n_src = 1;
 	config->number_of_source = n_src;
